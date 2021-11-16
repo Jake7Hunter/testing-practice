328026a17b3c91b6bef9c1f99a9aca5c7df1f1f9
Jake Hunter <jake7hunter@hmail.com>
2021-11-16 10:46:37 -0500

    Completed quotient testing

--- 01-unit/04-quotient.js
+++ 01-unit/04-quotient.js
@@ -1,7 +1,7 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function quotient(num1, num2 = 1) {
+function quotient(num1 = 0, num2 = 1) {
   if (num2 == 0) {
     return `Error`;
   }
@@ -44,7 +44,9 @@ try {
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
-  
+  var result = quotient();
+  if (result !== 0) throw new Error('Expected quotient() to be 0. Received: ' + result);
+
   console.log('All tests passed successfully.');
 
 // ==================================================
