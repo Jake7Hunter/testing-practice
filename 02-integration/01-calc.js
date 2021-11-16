17889767a5e7e89b14dd83c0b090ccfe086cd601
Jake Hunter <jake7hunter@hmail.com>
2021-11-16 11:00:02 -0500

    Defined functions to complete test 1

--- 02-integration/01-calc.js
+++ 02-integration/01-calc.js
@@ -1,7 +1,14 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function calc() {
+function sum(num1 = 0, num2 = 0) {
+  return num1 + num2;
+}
+
+function calc(Operation, num1, num2) {
+  switch(Operation){
+    case 'add': return sum(num1, num2)
+  }
 
 }
 
