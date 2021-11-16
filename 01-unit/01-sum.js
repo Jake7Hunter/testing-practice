25762363c5bec7dd9b5d8536eb51dce22a063f80
Jake Hunter <jake7hunter@hmail.com>
2021-11-16 09:23:56 -0500

    Added default 0 to variables for test 3

--- 01-unit/01-sum.js
+++ 01-unit/01-sum.js
@@ -1,7 +1,7 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function sum(num1, num2) {
+function sum(num1 = 0, num2 = 0) {
   return num1 + num2;
 }
 
@@ -26,7 +26,8 @@ try {
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+  var result = sum(1);
+  if (result !== 1) throw new Error('Expected sum(1) to be 1 Received: ' + result);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
