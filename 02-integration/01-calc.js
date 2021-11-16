529b9a1d3f8c2add3f72f4e9baa09ab62a8b4218
Jake Hunter <jake7hunter@hmail.com>
2021-11-16 11:11:51 -0500

    Defined quotient function to complete test 4

--- 02-integration/01-calc.js
+++ 02-integration/01-calc.js
@@ -13,11 +13,19 @@ function product(num1 = 0, num2 = 1) {
   return num1 * num2;
 }
 
+function quotient(num1 = 0, num2 = 1) {
+  if (num2 == 0) {
+    return `Error`;
+  }
+  return num1 / num2;
+}
+
 function calc(Operation, num1, num2) {
   switch(Operation){
     case 'add': return sum(num1, num2);
     case 'subtract': return difference(num1, num2);
     case 'multiply': return product(num1, num2);
+    case 'divide': return quotient(num1, num2);
   }
 
 }
@@ -52,6 +60,8 @@ try {
   // Test Case 4
   // --------------------------------------------------
   // It should return the correct quotient when the user provides: 'divide', 9, 3.
+  var result = calc('divide', 9, 3);
+  if (result !== 3) throw new Error('Expected calc("divide", 9, 3) to be 3. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 5
